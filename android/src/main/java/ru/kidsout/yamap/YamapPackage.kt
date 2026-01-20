b54daa2fde34a752ad27f9398166f866c5c04d52
Vyrtsev Mikhail <reeywhaar@gmail.com>
2026-01-20 04:37:26 +0400

    Add suggests

--- android/src/main/java/ru/kidsout/yamap/YamapPackage.kt
+++ android/src/main/java/ru/kidsout/yamap/YamapPackage.kt
@@ -6,20 +6,22 @@ import com.facebook.react.bridge.ReactApplicationContext
 import com.facebook.react.module.annotations.ReactModule
 import com.facebook.react.module.model.ReactModuleInfo
 import com.facebook.react.module.model.ReactModuleInfoProvider
+import ru.kidsout.yamap.suggest.YamapSuggestsModule
 import java.util.HashMap
 
-class YamapGeocodePackage : BaseReactPackage() {
+class YamapPackage : BaseReactPackage() {
   override fun getModule(name: String, reactContext: ReactApplicationContext): NativeModule? {
     return when (name) {
       YamapModule.NAME -> YamapModule(reactContext)
       YamapGeocodeModule.NAME -> YamapGeocodeModule(reactContext)
+      YamapSuggestsModule.NAME -> YamapSuggestsModule(reactContext)
       else -> null
     }
   }
 
   override fun getReactModuleInfoProvider(): ReactModuleInfoProvider {
     val moduleList: Array<Class<out NativeModule?>> =
-      arrayOf(YamapModule::class.java, YamapGeocodeModule::class.java)
+      arrayOf(YamapModule::class.java, YamapGeocodeModule::class.java, YamapSuggestsModule::class.java )
     val reactModuleInfoMap: MutableMap<String, ReactModuleInfo> = HashMap()
     for (moduleClass in moduleList) {
       val reactModule = moduleClass.getAnnotation(ReactModule::class.java) ?: continue
